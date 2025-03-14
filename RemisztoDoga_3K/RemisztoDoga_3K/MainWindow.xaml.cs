760e0e9f9573b4ea5c17e8b76167c2309cd58c48
KrizsakKornelKaroly <98745094+KrizsakKornelKaroly@users.noreply.github.com>
2025-03-14 10:15:38 +0100

    Neszemindenfogjameglyol

---
+++ RemisztoDoga_3K/RemisztoDoga_3K/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemisztoDoga_3K
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string nevTar = nev.Text;
+            string emailTar = email.Text;
+            string szulTar = szuldatum.Text;
+            string telTar = telefonszam.Text;
+            
+            NameCardWindow nameCardWindow = new NameCardWindow(nevTar, emailTar,  szulTar, telTar);
+            nameCardWindow.ShowDialog();
+        }
+
+        private void colors_Click(object sender, RoutedEventArgs e)
+        {
+            ColorSliderWindow colorSliderWindow = new ColorSliderWindow();
+            colorSliderWindow.ShowDialog();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ RemisztoDoga_3K/RemisztoDoga_3K/NameCardWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RemisztoDoga_3K
+{
+    /// <summary>
+    /// Interaction logic for NameCardWindow.xaml
+    /// </summary>
+    public partial class NameCardWindow : Window
+    {
+        public NameCardWindow(string nev, string email, string szuldat, string tel)
+        {
+            InitializeComponent();
+            Bepotyog(nev, email, szuldat, tel);
+        }
+
+        private void Bepotyog(string nev, string email, string szuldat, string tel)
+        {
+            cardName.Text = nev;
+            cardEmail.Text = email;
+            cardBirthDate.Text = szuldat;
+            cardPhoneNumber.Text = tel;
+        }
+    }
+}
